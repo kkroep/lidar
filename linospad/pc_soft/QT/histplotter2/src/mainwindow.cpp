c88d7db794763bd86970cf57339bd297ac5fef4d
kkroep <k.kroep1@gmail.com>
2016-07-02 18:04:46 +0200

    pc edit

--- linospad/pc_soft/QT/histplotter2/src/mainwindow.cpp
+++ linospad/pc_soft/QT/histplotter2/src/mainwindow.cpp
@@ -1278,7 +1278,7 @@ void MainWindow::on_savePreviewButton_clicked()
     QString histfilename = QFileDialog::getSaveFileName(this,"Choose save file name line 1277","results/");
     cerr << "debug 1279 tester." << endl;
 
-    //QString new_file_location = "garbage.txt";
+    QString new_file_location = "garbage.txt";
     cerr << "debug 1282 tester." << endl;
     //cerr << "Debug 1283 " << new_file_location << endl;
 
