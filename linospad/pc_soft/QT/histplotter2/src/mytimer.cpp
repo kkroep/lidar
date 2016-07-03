f3bc3bef80876f76910f69a8e2fba7e8c2d89c56
kkroep <k.kroep1@gmail.com>
2016-07-03 18:32:34 +0200

    pc edit

--- linospad/pc_soft/QT/histplotter2/src/mytimer.cpp
+++ linospad/pc_soft/QT/histplotter2/src/mytimer.cpp
@@ -1,6 +1,27 @@
-#include "mytimer.h"
-#include <QtCore>
+// #include "mytimer.h"
+// #include <QtCore>
+// #include <QDebug>
+
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include "qcustomplot.h"
+#include "mytimer.h" // kees added this
 #include <QDebug>
+#include <QDesktopWidget>
+#include <QScreen>
+#include <QMessageBox>
+#include <QMetaEnum>
+#include <QStatusBar>
+#include <QFileDialog>
+#include <QSettings>
+#include <QChar>
+#include <QShortcut>
+#include <QTimer>
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <complex>
+#include <algorithm>
 
 MyTimer::MyTimer()
 {
