07e2877b8a5eeb49e23085db32cd0398cff8869c
kkroep <k.kroep1@gmail.com>
2016-07-03 18:49:28 +0200

    pc edit

--- linospad/pc_soft/QT/histplotter2/src/mainwindow.cpp
+++ linospad/pc_soft/QT/histplotter2/src/mainwindow.cpp
@@ -1287,12 +1287,12 @@ void MainWindow::on_savePreviewButton_clicked()
     // cerr << "debug 1282 tester. folder: " << folder_number << "   file: " << file_number << endl;
     //QString histfilename = QFileDialog::getSaveFileName(this,"Choose save file name line 1277","results/");
 
-    timer_kees = new QTimer();
+//     timer_kees = new QTimer();
 //     QObject::connect(timer_kees, SIGNAL(timeout()), this, SLOT(onTimeout()));
- 
-    int msec = 200;
-    timer_kees->setInterval(msec);
-    timer_kees->start();
+//  
+//     int msec = 200;
+//     timer_kees->setInterval(msec);
+//     timer_kees->start();
 
 
 
