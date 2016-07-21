b54712d7452fdda34c4fc863c96f9a86ee147fb5
kkroep <k.kroep1@gmail.com>
2016-07-21 15:47:47 -0700

    TDC sorted plots

---
+++ radiation_doc/measurement/TDC_sorted.m
@@ -0,0 +1,73 @@
+% octave file for postprocessing info
+clear all;
+close;
+clc;
+
+
+hours =5;
+minutes = 60;
+range = [417:448];
+
+for k=range
+
+	for i=1:hours
+		folder_loc = 'h_';
+		folder_loc = 	[folder_loc int2str(i)];
+		% disp(folder_loc);
+
+		for j=1:minutes
+			file_loc = [folder_loc '/m_' int2str(j) '.txt'];
+			% disp(file_loc);5
+			a = csvread(file_loc);
+			b(k,(i-1)*minutes+j) = a(k);
+		end
+	end
+end
+%
+%for k=1:no_SPADs
+%	staven(k) = sum(b(k,:));
+%end
+%
+%bar(staven);
+%print -deps bars.eps;
+
+%close;
+
+max_lines = 3;
+
+
+no_lines = max_lines;
+hold on;
+y_max = -realmax;
+y_min = 0;
+
+for k=range
+	plot(1:length(b(k,:)),b(k,:), 'k');
+  if(max(b(k,:))>y_max)
+    y_max = max(b(k,:));
+  end
+	fprintf('%d, ', k);
+end
+
+fprintf('\n\n');
+
+x = [15 116];
+for i=x
+  plot([i,i],[y_min,y_max],'Linewidth',2,'--', 'color','r');
+end
+
+x = 144;
+for i=x
+  plot([i,i],[y_min,y_max],'Linewidth',2,'--', 'color','b');
+end
+
+x = [152 251];
+for i=x
+  plot([i,i],[y_min,y_max],'Linewidth',2,'--', 'color','g');
+end
+
+hold off;
+print -deps 'spad_417-448.eps';
+
+
+
--- radiation_doc/measurement/postprocessor.m
+++ radiation_doc/measurement/postprocessor.m
@@ -9,11 +9,11 @@ minutes = 60;
 
 % base measurement
 a = csvread('h_1/m_1.txt');
-base_line = sum(a);
-base_line = 0;
 
 
-for i=4:hours
+
+
+for i=1:hours
 	folder_loc = 'h_';
 	folder_loc = 	[folder_loc int2str(i)];
 	% disp(folder_loc);
@@ -22,11 +22,34 @@ for i=4:hours
 		file_loc = [folder_loc '/m_' int2str(j) '.txt'];
 		% disp(file_loc);
 		a = csvread(file_loc);
-		b((i-1)*minutes+j) = sum(a)-base_line;
+		b((i-1)*minutes+j) = sum(a);
 	end
 end
 
 
 
-plot(1:length(b),b);
+plot(1:length(b),b, 'k');
+
+y_min = 0;
+y_max = max(b);
+
+hold on;
+x = [15 116];
+for i=x
+  plot([i,i],[y_min,y_max],'Linewidth',2,'--', 'color','r');
+end
+
+x = 144;
+for i=x
+  plot([i,i],[y_min,y_max],'Linewidth',2,'--', 'color','b');
+end
+
+x = [152 251];
+for i=x
+  plot([i,i],[y_min,y_max],'Linewidth',2,'--', 'color','g');
+end
+hold off;
+
+legend('combined DCR','60 MeV start','60 MeV stop','reposition device','10 MeV start','10 MeV stop')
+
 print -deps test.eps;
