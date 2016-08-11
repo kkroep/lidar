0b36faf932ab9333388cd07aaff59a787a953f3a
kkroep <k.kroep1@gmail.com>
2016-08-11 11:30:37 -0700

    a lot of work on tables

--- octave/photons_per_SPAD_tab.m
+++ octave/photons_per_SPAD_tab.m
@@ -7,13 +7,13 @@ A{1,1} = sprintf('\\textbf{photons per SPAD}');
 A{1,2} = sprintf('');
 
 A{2,1} = sprintf('$PPS_B$');
-A{2,2} = number_converter_exp(PPSS_B, ' \text{photon}/s');
+A{2,2} = number_converter_exp(PPSS_B, ' \text{counts}/s');
 
 A{3,1} = sprintf('$PPS_N$');
-A{3,2} = number_converter_exp(PPSS_N, ' \text{photon}/s');
+A{3,2} = number_converter_exp(PPSS_N, ' \text{counts}/s');
 
 A{4,1} = sprintf('$PPS_S$');
-A{4,2} = number_converter_exp(PPSS_S, ' \text{photon}/s/W');
+A{4,2} = number_converter_exp(PPSS_S, ' \text{counts}/s/W');
 
-caption = sprintf('Amount of backgroun (B), dark count (N), and signal (S) photons that hit a single SPAD per second');
+caption = sprintf('Amount of backgroun (B), dark count (N), and signal (S) counts that are observed by a single SPAD per second');
 latextable(A, name, tab_path, caption);
